9853144986fc53a1e0db1f2c4de183605a2c9e4a
Tomoshige Nakamura <tomoshige.nakamura@gmail.com>
2025-01-18 18:27:05 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='bayesian_did',
+    version='0.1.0',
+    description='A Bayesian difference-in-difference module with effect heterogeneity.',
+    author='Your Name',        # ここはご自身の名前に
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'seaborn'
+    ],
+    python_requires='>=3.7'
+)
